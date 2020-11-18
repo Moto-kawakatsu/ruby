4f4fddb6aab0b401e09897d089595e5545d9569d
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-18 22:57:35 +0900

    じゃんけんの勝ち負けに関する条件分岐

--- jyanken.rb
+++ jyanken.rb
@@ -1,7 +1,14 @@
-DEFEATS = {グー: :チョキ, チョキ: :パー, パー: :グー}
+DEFEATS = { グー: :チョキ, チョキ: :パー, パー: :グー }
 THROWS = DEFEATS.keys
 
 puts "グー、チョキ、パーのどれかを入力してください"
-puts "じゃんけん...#{@user_throw - gets.chomp.to_sym}"
+puts "じゃんけん...#{@user_throw = gets.chomp.to_sym}"
 puts "コンピュータは#{cpu_throw = THROWS.sample}を出しました"
 
+if @user_throw == @cpu_throw
+    puts "あいこでした。もう一度行ってください"
+elsif @cpu_throw == DEFEATS[@user_throw]
+    puts "あなたの負けです"
+else
+    print "あなたの勝ちです！！"    
+end
