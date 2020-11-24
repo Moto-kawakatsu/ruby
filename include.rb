1cf2f5255ab6f75caed8c7c3c569a5d07e1d314b
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-24 22:28:55 +0900

    includeメソッドの学習

---
+++ include.rb
@@ -0,0 +1,10 @@
+def array123(nums)
+    if nums.include?(1) && nums.include?(2) && nums.include?(3)
+      puts "True"
+    else
+      puts "False"
+    end
+  end
+  
+
+  array123([2,6,4,8,9,0,11,24,662,785,789,3,1])
